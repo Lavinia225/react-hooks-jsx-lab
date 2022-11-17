ecf5accb56e23450dd644f568030db5dfe0ec71d
Luna Seifert <thepuddingdeity@gmail.com>
2022-11-17 16:09:28 -0700

    commit tree 81f10d79e4292a7cb74df74f4a4ab4a446b76db4

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,14 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const colorStyle = {
+    color: 'firebrick'
+  }
+
+  return <div id="home">Home
+    <h1 style={colorStyle}>{name} is a Web Developer from {city}</h1>
+  </div>;
 }
 
 export default Home;
+
